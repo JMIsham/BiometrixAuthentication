a5e07aca7bbb6bbf2078c77ff68826c194e4a55f
JMIsham <isham1002@gmail.com>
2017-03-08 23:46:53 +0530

    Beat version completedgit add *!

--- src/boimetrixauthentication/Model/BoimetrixAuthentication.java
+++ src/boimetrixauthentication/Model/BoimetrixAuthentication.java
@@ -16,40 +16,67 @@ public class BoimetrixAuthentication {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
-        int[] ints={150,90,62,56,12,89,15,40};
-        int[] ints2={150,90,62,56,12,87,15,42};
-        int[] ints3={150,90,62,56,12,89,15,44};
-//        System.out.println((IOManager.Read("user2")));
-        System.out.println(register("140236",ints ));
-        System.out.println(authenticate("140236", ints2));
-        System.out.println(authenticate("140236", ints3));
-        System.out.println(authenticate("140233", ints2));
-   
-    }
+//    public static void main(String[] args) {
+//        
+//        int[] ints={100,100,100,100,100,100,100,100};
+//        int[] ints2={150,90,62,56,12,87,15,42};
+//        int[] ints3={150,90,62,56,12,89,15,44};
+////        System.out.println((IOManager.Read("user2")));
+//        System.out.println(authenticate("isham2",ints ));
+//        System.out.println(authenticate("140236", ints2));
+//        System.out.println(authenticate("140236", ints3));
+//        System.out.println(authenticate("140233", ints2));
+//   
+//    }
     public static String register(String username,int[] handFeaters){
-        if(handFeaters.length!=8)return "INVALID!";
+        if(username.length()<1) return "INVALID USERNAME!";
+        String inputStatus = checkFeatures(handFeaters);
+        String usernameStatus = checkUsername(username);
+        if(!"OK".equals(inputStatus)) return inputStatus;
+        if(!"OK".equals(usernameStatus)) return usernameStatus;
         String code = EncryptionEngine.encrypt(handFeaters);
         String data = (username+" : "+code);
         String response = IOManager.Write(data);
         return response;
     }
     public static String authenticate(String username,int[] handFeaters){
+        if(username.length()<1) return "INVALID USERNAME!";
+        String inputStatus = checkFeatures(handFeaters);
+        if(!"OK".equals(inputStatus)) return inputStatus;
         String response = IOManager.Read(username);
         if ("USER_NOT_AVILABLE".equals(response) || "LOGIN_FAILED".equals(response) )return response;
         int[] data = EncryptionEngine.decrypt(response);
         return validate(data, handFeaters);
     }
     private static String validate(int[] a , int[] b){
-       int fails = 0;
         for(int i=0;i<a.length;++i){
             if((Math.abs(a[i]-b[i]))>3){
-                fails++;
+                return "AUTHENTICATION_FAILED";
             }
         }
-        if (fails>3)return "AUTHENTICATION_FAILED";
+        
         return "SUCCEEDED!";
+    }
+    public static boolean checkUsernameFromStore(String username){
+        String response = IOManager.Read(username);
+        if ("USER_NOT_AVILABLE".equals(response)) return true;
+        return false;
+    }
+    private static String checkUsername(String username){
+        String usernamestriped = username.replaceAll("\\s+","");
+        usernamestriped = usernamestriped.replaceAll("[-+.^:,]","");
+        if(!username.equalsIgnoreCase(usernamestriped)) return "INVALID USERNAME";
+        if (!BoimetrixAuthentication.checkUsernameFromStore(username)) return "USERNAME AlREADY EXISTS!";
+        return "OK";
+    }
+    private static String checkFeatures(int[] values){
+        if(values.length!=8) return "INVALID_INPUT";
+        for (int value : values) {
+            if(value<3 || value>250){
+                return "INVALID_INPUT";
+            }
+        }
+        return "OK";
     }
     
 }
